684e7f46737da16f9a750ac22702cadd82c883ce
Augusto Akira Miasato <augusto.akira08@hotmail.com>
2024-04-13 17:25:13 -0300

    Alteracoes faro

---
+++ controller/bonus.php
@@ -0,0 +1,41 @@
+ <?php
+ include "../view/(8)bonus.html";
+ if(isset($_POST['submit'])){
+        
+        $totalvendas = floatval($_POST['totalvendas']);
+        $totalvendedores = floatval($_POST['totalvendedores']);
+
+        // Calcula o custo de produção (40% do total de vendas)
+        $custo_producao = $totalvendas * 0.4;
+
+        // Calcula o lucro (60% do total de vendas)
+        $lucro = $totalvendas * 0.6;
+
+        // Calcula a participação dos funcionários (10% do lucro)
+        $participacao = $lucro * 0.1;
+
+        // Calcula o bônus (4% do lucro)
+        $bonus = $lucro * 0.04;
+
+        // Calcula o valor total a ser distribuído entre os funcionários (participação + bônus)
+        $total_distribuido = $participacao + $bonus;
+
+        // Calcula o valor pago a cada funcionário
+        $valor_por_funcionario = $total_distribuido / $totalvendedores;
+
+        // Exibe os resultados na tela
+    
+    echo
+    "<div class='card mt-4 bg-light ' style='width: 18rem'>
+    <div class='card-body'>
+        <p class='card-text text-dark'>
+        Total do custo de produção: R$$custo_producao   <br>
+        Total do lucro: R$$lucro   <br>
+        Valor total de participação distribuído aos funcionários: R$$participacao  <br>
+        Valor total do bônus: R$$bonus <br>
+        Valor pago a cada um dos funcionários: R$$valor_por_funcionario <br>
+        </p>
+    </div>
+    </div>";
+        }
+    
---
+++ controller/conversaohoras.php
@@ -0,0 +1,35 @@
+<?php
+include "../view/(10)conversaohoras.html";
+    if (isset($_POST['submit'])) {
+        $horas = intval($_POST['horas']);
+        $min= intval($_POST['min']);
+        $seg = intval($_POST['seg']);
+
+    if ($min < 0 || $min > 59) {
+        echo "<p class='text-danger'>Os minutos devem estar entre 0 e 59.</p>";
+        exit();
+    }
+
+    if ($seg < 0 || $seg > 59) {
+        echo "<p class='text-danger'>Os segundos devem estar entre 0 e 59.</p>";
+        exit();
+    }
+
+        $seg1 = $horas * 60 * 60;
+        $seg2 = $min * 60;
+        
+
+        $totalseg= $seg + $seg1 + $seg2;
+
+
+        echo
+"<div class='card mt-4 bg-light ' style='width: 18rem'>
+<div class='card-body'>
+    <p class='card-text text-dark'>
+    $horas horas deram $seg1 segundos <br>
+    $min minutos deram $seg2 segundos <br>
+    Logo a hora $horas h$min m$seg s deram ao todo $totalseg segundos <br>
+    </p>
+</div>
+";
+    }
