a86ef2ef55ef33f7761aab5233bdd332f3615070
miyazatoakira <augusto.akira08@hotmail.com>
2024-06-10 22:23:56 -0300

    Exercicios corrigidos

--- controller/(1)tabuada.php
+++ controller/(1)tabuada.php
@@ -2,7 +2,10 @@
 include "../view/exerciciosLoops/(1)tabuada.html";
 $augusto = intval($_POST['num1']);
 
+echo '<ul class="text-center">';
 for ($i = 1; $i <= 20; $i++) {
     echo '<li>' . $augusto . ' x ' . $i . ' = ' . ($augusto * $i) . '</li>';
 }
-echo"<p class="text-center"> Feito por Augusto Akira Miasato </p>"
+echo '</ul>';
+echo '<p class="text-center">Feito por Augusto Akira Miasato</p>';
+?>
--- controller/(2)imparPar.php
+++ controller/(2)imparPar.php
@@ -1,28 +1,27 @@
 <?php
 include "../view/exerciciosLoops/(2)parImpar.html";
 $valores = [];
-for($i = 1; $i<=10; $i++){
+for ($i = 1; $i <= 10; $i++) {
     $valores[] = $_POST['num' . $i];
 }
 $valPar = [];
 $valImpar = [];
 
-foreach($valores as $val){
-    if ($val % 2 == 0){
+foreach ($valores as $val) {
+    if ($val % 2 == 0) {
         $valPar[] = $val;
-    }
-    if ($val % 2 != 0){
+    } else {
         $valImpar[] = $val;
     }
 }
 
-echo
-        "<div class='card mt-4 bg-light text-center align-self-center' style='width: 18rem'>
-            <div class='card-body'>
-                <p class='card-text text-dark'>
-                Valores Pares: ". implode(" ,", $valPar);" / 
-                Valores Impar: " . implode(" ,", $valImpar);"
-                </p>
-            </div>
-        </div><p class='align-self-center mt-5'>Feito por Augusto Akira Miasato</p>";
-    ;
+echo "<div class='card mt-4 bg-light text-center align-self-center' style='width: 18rem'>
+        <div class='card-body'>
+            <p class='card-text text-dark'>
+                Valores Pares: " . implode(", ", $valPar) . "<br>
+                Valores Ímpares: " . implode(", ", $valImpar) . "
+            </p>
+        </div>
+      </div>
+      <p class='align-self-center mt-5'>Feito por Augusto Akira Miasato</p>";
+?>
