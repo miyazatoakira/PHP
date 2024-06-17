2bbd43507aa82f2636b604ab56d2c76d50db3b0f
Augusto Akira Miasato <149327671+miyazatoakira@users.noreply.github.com>
2024-06-17 09:17:52 -0300

    Atualizar o (6)numeroPrimo.php

--- controller/(6)numeroPrimo.php
+++ controller/(6)numeroPrimo.php
@@ -5,24 +5,26 @@ $num1 = intval($_POST['num1']);
 $isPrime = "não é Primo";
 $i = $num1;
 do{
-    if(($num1 % $i) == 0 ){
-        $isPrime = "é Primo";
-    }
-    if($num1 == 0){
+    if($num == 0){
         $isPrime = "é nulo";
     }
-    else{
-        $isPrime = "não é Primo";
+    if($num % $i){
+        $isPrime += 1;
     }
     $i--;
 }
 while($i!=2);
-
+if($isPrime == 2){
+  $resultado = "é Primo";
+}
+else if($isPrime > 2){
+  $resultado = "não é Primo";
+}
 echo
 "<div class='card mt-4 bg-light text-center position-absolute start-50 translate-middle ' style='width: 18rem'>
     <div class='card-body'>
         <p class='card-text text-dark'>
-            O número  $isPrime
+            O número  $resultado
         </p>
     </div>
     <p class='align-self-center mt-5'>Feito por Augusto Akira Miasato</p>
