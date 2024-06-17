612d4430259c912ea66b1d32802e8bf024f3620c
miyazatoakira <augusto.akira08@hotmail.com>
2024-06-17 08:13:18 -0300

    Inicio Exercicios Repeticao

--- controller/(1)tabuada.php
+++ controller/(1)tabuada.php
@@ -2,7 +2,7 @@
 include "../view/exerciciosLoops/(1)tabuada.html";
 $augusto = intval($_POST['num1']);
 
-echo '<a href="../exerciciosLoops/(1)tabuadas.html" class="btn btn-primary">Deseja inserir outro número ?</a>'
+echo '<a href="../exerciciosLoops/(1)tabuadas.html" class="btn btn-primary">Deseja inserir outro número ?</a>';
 echo '<ul class="text-center mt-5">';
 for ($i = 1; $i <= 20; $i++) {
     echo '<li>' . $augusto . ' x ' . $i . ' = ' . ($augusto * $i) . '</li>';
